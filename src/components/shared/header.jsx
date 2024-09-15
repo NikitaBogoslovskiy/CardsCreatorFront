02f0f9fd03b5f0a9dcaae59c106af64b9fe248a3
Nikita Bogoslovskiy <nikita-beeline@yandex.ru>
2024-09-15 21:52:20 +0300

    add home link to title and logo

--- src/components/shared/header.jsx
+++ src/components/shared/header.jsx
@@ -7,47 +7,64 @@ import {
     Typography,
     Icon,
     Box,
-    Button
+    Button,
+    IconButton,
+    SvgIcon,
+    Link
 } from "@mui/material";
 import LogoImg from "../../../public/images/logo.png"
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LoginIcon from '@mui/icons-material/Login';
 import { theme } from "../../themes/custom.jsx"
 
-  
 function Header(props) {
     const navigation = ["home", "articles", "contact"];
 
     return (
         <AppBar sx={{ bgcolor: theme.background.default, display: "block", position: "sticky" }}>
-            <Toolbar>
-                <Icon component="div" sx={{ mr: 1.5, mt: 0.3, display: { xs: "none", sm: "block" } }}>
-                    <img src={LogoImg} height={25} />
-                </Icon>
-                <Typography
-                    component="div"
-                    sx={{
-                        flexGrow: 1,
-                        display: { xs: "none", sm: "block" }, 
-                        color: theme.text.header.color, 
-                        fontSize: theme.text.header.size.content,
-                        fontWeight: theme.text.header.weight, 
-                        fontFamily: theme.text.header.family,
-                    }}
-                >
-                    Cards Creator
-                </Typography>
-                <Box component="nav" sx={{ display: { xs: "none", sm: "block" } }}>
-                    {navigation.map((item) => (
-                        <Button key={item} href={"/" + item} 
-                        sx={{ 
-                            color: theme.text.primary.color, 
-                            fontSize: theme.text.primary.size.button, 
-                            fontFamily: theme.text.primary.family, 
-                            fontWeight: theme.text.primary.weight 
-                        }}>
-                            {item}
-                        </Button>
-                    ))}
-                </Box>
+            <Toolbar sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                <Container disableGutters sx={{ margin: 0, display: { xs: "none", sm: "block" } }}>
+                    <Container disableGutters sx={{ margin: 0, display: "flex", flexDirection: "row", justifyContent: "left", alignItems: "center" }}>
+                        <Link href="/home" underline="none">
+                            <Icon component="div" sx={{ mr: "10px", display: "flex", flexDirection: "column", justifyContent: "center" }}>
+                                <img src={LogoImg} height={25} />
+                            </Icon>
+                        </Link>
+                        <Link href="/home" underline="none">
+                            <Typography
+                                component="div"
+                                sx={{
+                                    userSelect: "none",
+                                    display: { xs: "none", sm: "block" }, 
+                                    color: theme.text.header.color, 
+                                    fontSize: theme.text.header.size.content,
+                                    fontWeight: theme.text.header.weight, 
+                                    fontFamily: theme.text.header.family,
+                                }}
+                            >
+                                Cards Creator
+                            </Typography>
+                        </Link>
+                    </Container>
+                </Container>
+                <Container disableGutters sx={{ margin: 0, display: { xs: "none", sm: "block" } }}>
+                    <Container disableGutters component="nav" sx={{ margin: 0, display: "flex", flexDirection: "row", justifyContent: "right", wrap: "nowrap" }}>
+                        {navigation.map((item) => (
+                            <Button key={item} href={"/" + item} 
+                            sx={{ 
+                                color: theme.text.primary.color, 
+                                fontSize: theme.text.primary.size.button, 
+                                fontFamily: theme.text.primary.family, 
+                                fontWeight: theme.text.primary.weight 
+                            }}>
+                                {item}
+                            </Button>
+                        ))}
+                        <IconButton onClick={() => {}}>
+                            <SvgIcon component={LoginIcon} sx={{ color: "#000000" }}/>
+                        </IconButton>
+                    </Container>
+                </Container>
             </Toolbar>
         </AppBar>
     )
